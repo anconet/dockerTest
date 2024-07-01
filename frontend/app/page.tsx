8791d09808d8f106871d3dcc2805b4bdbef4dc60
Anthony Anconetani <anthony.anconetani@gmail.com>
2024-07-01 17:57:28 -0500

    getting rid of the old frontend.

--- frontend/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import App from "./App";
-export default function Page() {
-    console.log("to Page")
-    return <App></App>
-}
--- frontend2/app/page.tsx
+++ frontend2/app/page.tsx
@@ -4,11 +4,10 @@ import { getSession } from "./welcome/authorization";
 
 export default async function Main() {
   console.log("Main Page")
-  //const session = cookies().get("session")?.value
-  //console.log("Cookie:", session)
-  //if (session !== "Hayden") { redirect("/welcome") }
+
   const session = await getSession()
   console.log(session)
+
   if (session === null) { redirect("/welcome") }
   return (
     < p > Main Page</p >
